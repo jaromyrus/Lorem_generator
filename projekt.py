34682942d3765309438a4e9c1c119176e2fd12f8
jaromyrus <147605462+jaromyrus@users.noreply.github.com>
2023-12-12 16:53:59 +0100

    druhy commit

--- projekt.py
+++ projekt.py
@@ -11,13 +11,12 @@ def generate_lorem_ipsum():
     lorem_ipsum_text = ""
 
     lorem_ipsum_text += "Lorem ipsum "
-    for _ in range(slova_na_odstavec - 2):  # -2 because we already added "Lorem ipsum" at the beginning
+    for _ in range(slova_na_odstavec - 2):
         slovo = random.choice(lorem_sluvka)
         lorem_ipsum_text += slovo + " "
     lorem_ipsum_text += ".\n"
 
-    # Následující odstavce obsahují pouze náhodná slova
-    for _ in range(odstavce - 1):  # -1 because the first paragraph is already added
+    for _ in range(odstavce - 1):  
         for _ in range(slova_na_odstavec):
             slovo = random.choice(lorem_sluvka)
             lorem_ipsum_text += slovo + " "
@@ -25,6 +24,5 @@ def generate_lorem_ipsum():
 
     return lorem_ipsum_text
 
-# Generování Lorem Ipsum textu
 generated_text = generate_lorem_ipsum()
 print(generated_text)
