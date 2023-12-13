d64c7551eff0a08f2053d0d621977d1d6182245e
jaromyrus <147605462+jaromyrus@users.noreply.github.com>
2023-12-13 21:45:07 +0100

    treti commit

--- projekt.py
+++ projekt.py
@@ -4,24 +4,24 @@ def generate_lorem_ipsum():
     odstavce = int(input("Zadejte počet odstavců: "))
     slova_na_odstavec = int(input("Zadejte počet slov na odstavec: "))
 
-    lorem_sluvka = ["morem", "kipsum", "dvere", "sitě", "samet,", "consikulovat", "adipisciovat", "elita",
+    lorem_sluvka = ["morem", "kipsum", "dvere", "sitě", "samet", "consikulovat", "adipisciovat", "elita",
                    "sedleh", "do", "eiusmod", "temporarne", "incididuntit", "tutat", "laborete", "eta", "doloreice", "kockopes", "katzehunde", "kous", "špíluser", "komanditista", "tichykamos", "smegrus",
                    "mononukleousus", "hladus", "potisse", "rychlous", "morus", "modius", "morbius", "stolus", "negrus", "venizitiri"]
     
     lorem_ipsum_text = ""
 
     lorem_ipsum_text += "Lorem ipsum "
-    for _ in range(slova_na_odstavec - 2):
+    for i in range(slova_na_odstavec - 2): 
         slovo = random.choice(lorem_sluvka)
         lorem_ipsum_text += slovo + " "
     lorem_ipsum_text += ".\n"
 
-    for _ in range(odstavce - 1):  
-        for _ in range(slova_na_odstavec):
+    for i in range(odstavce - 1): 
+        for i in range(slova_na_odstavec):
             slovo = random.choice(lorem_sluvka)
             lorem_ipsum_text += slovo + " "
         lorem_ipsum_text += ".\n"
-
+    lorem_ipsum_text = "\n".join([line.capitalize() for line in lorem_ipsum_text.split("\n")])
     return lorem_ipsum_text
 
 generated_text = generate_lorem_ipsum()
